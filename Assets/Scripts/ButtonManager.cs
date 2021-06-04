b46123b05df187317931f62ee117909982993ce9
Andres Maturin <idontknowwhattonamethiskhay@gmail.com>
2021-06-04 14:36:45 -0700

    Added a button to go to the weapons scene

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -7,12 +7,15 @@ public class ButtonManager : MonoBehaviour
 {
 
     private string playSceneName = "TempPlay";
+    private string weaponScene = "TempShooting";
 
     public void LoadPlay(){
         SceneManager.LoadScene(playSceneName);
     }
 
-    
+    public void LoadWeapons(){
+        SceneManager.LoadScene(weaponScene);
+    }
     // Start is called before the first frame update
     void Start()
     {

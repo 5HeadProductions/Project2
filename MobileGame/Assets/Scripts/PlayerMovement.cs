6ccdd10006c7a8dc90e4112a44875538eae4709c
EdenArts <ojedaeden08@gmail.com>
2021-06-04 11:09:19 -0700

    player movement script

---
+++ MobileGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private LayerMask _aimLayerMask;
+
+    private Animator _animator;
+
+    private void Awake() => _animator = GetComponent<Animator>(); // => is an expression body methood
+
+    private void Update()
+    {
+        AimTowardsMouse();
+        
+        //reading Input
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        
+        //Moving
+        if (movement.magnitude > 0)
+        {
+            movement.Normalize();
+            movement *= _speed * Time.deltaTime;
+            transform.Translate(movement, Space.World);
+        }
+
+        float velocityZ = Vector3.Dot(movement.normalized, transform.forward);
+        float velocityX = Vector3.Dot(movement.normalized, transform.right);
+        
+        _animator.SetFloat("VelocityZ", velocityZ, 0.1f,Time.deltaTime);
+        _animator.SetFloat("VelocityX", velocityX, 0.1f,Time.deltaTime);
+    }
+
+    private void AimTowardsMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, _aimLayerMask))
+        {
+            var _direction = hitInfo.point - transform.position;
+            _direction.y = 0f;
+            _direction.Normalize();
+            transform.forward = _direction;
+        }
+    }
+}

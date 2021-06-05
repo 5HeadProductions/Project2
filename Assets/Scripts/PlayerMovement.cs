2eb230a9c0f94c7d77645bbe30f875d2543c4fde
EdenArts <ojedaeden08@gmail.com>
2021-06-05 10:50:49 -0700

    minor change

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -35,8 +35,8 @@ public class PlayerMovement : MonoBehaviour
         float velocityZ = Vector3.Dot(movement.normalized, transform.forward);
         float velocityX = Vector3.Dot(movement.normalized, transform.right);
         
-        _animator.SetFloat("VelocityZ", velocityZ, 0.1f,Time.deltaTime);
-        _animator.SetFloat("VelocityX", velocityX, 0.1f,Time.deltaTime);
+        //_animator.SetFloat("VelocityZ", velocityZ, 0.1f,Time.deltaTime);
+        //_animator.SetFloat("VelocityX", velocityX, 0.1f,Time.deltaTime);
     }
 
     private void AimTowardsMouse()

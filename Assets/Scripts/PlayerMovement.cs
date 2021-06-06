d64c549f169a3f0b765c8796385622e213cc4405
EdenArts <ojedaeden08@gmail.com>
2021-06-06 14:43:01 -0700

    cinemachine settings changed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,25 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
     [SerializeField] private LayerMask _aimLayerMask;
+    [SerializeField] Transform playerMovementDirection;
 
     private Animator _animator;
 
     public Joystick joystick;
 
+    
+
     private void Awake() => _animator = GetComponent<Animator>(); // => is an expression body methood
 
     private void Update()
     {
-        AimTowardsMouse();
+       // AimTowardsMouse();
+
+        playerMovementDirection.position = new Vector3(joystick.Horizontal + transform.position.x, -1.54f, joystick.Vertical + transform.position.z);
         
-        //reading Input for keyboard
-        //float horizontal = Input.GetAxis("Horizontal");
-        //float vertical = Input.GetAxis("Vertical");
+        transform.LookAt(new Vector3(playerMovementDirection.position.x, 0, playerMovementDirection.position.z));
+
+        transform.eulerAngles = new Vector3(0,transform.eulerAngles.y,0);
 
         Vector3 movement = new Vector3(joystick.Horizontal, 0f, joystick.Vertical);
         

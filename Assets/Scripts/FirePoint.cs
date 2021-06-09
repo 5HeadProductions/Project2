f9e2e8e99175acc3d04b9298d459b4eca7aef9c2
Andres Maturin <idontknowwhattonamethiskhay@gmail.com>
2021-06-09 13:45:01 -0700

    The zombie now takes damage from the bullets, the player earns one coin per shot landed on the basic enemy.
    Added a Player Manager script that keeps track of the players health and the players coins. I made the zombie player into a prefab. Created a HUD script which will display important things on the canvas for the user.

--- Assets/Scripts/FirePoint.cs
+++ Assets/Scripts/FirePoint.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// this script is attached to the weapons, blasterA
+// this script is attached to the weapons, blasterA, shoots the projectiles
 public class FirePoint : MonoBehaviour
 {
     public Transform firePoint; // gameobject in front of the gun where the projectile will spawn from

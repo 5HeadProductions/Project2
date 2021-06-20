dcc1e7fc191eb3bb46fce86a0a191f20da8dbec7
Andres Maturin <idontknowwhattonamethiskhay@gmail.com>
2021-06-20 14:12:07 -0700

    Added the guns to show on screen and have a button, button doesn't work yet

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -8,7 +8,11 @@ public class Inventory : MonoBehaviour
     [SerializeField] private Text coin_txt, gem_txt;
     [SerializeField]private int currentCoins, currentGems;
 
+    [SerializeField]private GameObject BottomGun, TopGun; // used to display the current gun equiped.
+    [SerializeField]private Texture2D bottomG,topG;
+    private Texture2D  holderG;
 
+    public Dictionary<string, GameObject> guns = new Dictionary<string, GameObject>();// store all the guns in the game
     private PlayerManager playerInstance;
     void Start(){
         playerInstance = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
@@ -32,5 +36,15 @@ public class Inventory : MonoBehaviour
             gem_txt.text = currentGems.ToString();
         }
         
+    }
+
+
+    public void Equipped(){ // bottom gun is the ewuipped gun
+    Debug.Log("HIHI");
+        holderG = topG; // the player pressed on the top gun to equip it
+        bottomG = topG; // currently eqioped gun goes to the top
+        bottomG = holderG; // top gun goes to the bottom
+
+
     }
 }

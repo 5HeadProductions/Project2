7006336cd00738e08c3f6031bff8594ad48d7964
Andres Maturin <idontknowwhattonamethiskhay@gmail.com>
2021-06-21 12:41:40 -0700

    The gun switches when the button is pressed, Added more kenny assets.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -8,19 +8,23 @@ public class Inventory : MonoBehaviour
     [SerializeField] private Text coin_txt, gem_txt;
     [SerializeField]private int currentCoins, currentGems;
 
-    [SerializeField]private GameObject BottomGun, TopGun; // used to display the current gun equiped.
-    [SerializeField]private Texture2D bottomG,topG;
-    private Texture2D  holderG;
+    [SerializeField]private GameObject bottomGunObject, topGunObject; // used to display the current gun equiped.
 
-    public Dictionary<string, GameObject> guns = new Dictionary<string, GameObject>();// store all the guns in the game
+    [SerializeField]private RawImage bottomGunImage, topGunImage;
+    private Texture  holderG;
+
+    //private GameObject[] gunInLevel = new GameObject[2];
+
+ //   public Dictionary<string, GameObject> guns = new Dictionary<string, GameObject>();// store all the guns in the game
     private PlayerManager playerInstance;
     void Start(){
         playerInstance = GameObject.Find("PlayerManager").GetComponent<PlayerManager>();
         currentCoins = playerInstance.coins;
         currentGems = playerInstance.gems;
-
         coin_txt.text = currentCoins.ToString();
         gem_txt.text = currentGems.ToString();
+
+
 
     }
 
@@ -39,11 +43,11 @@ public class Inventory : MonoBehaviour
     }
 
 
-    public void Equipped(){ // bottom gun is the ewuipped gun
-    Debug.Log("HIHI");
-        holderG = topG; // the player pressed on the top gun to equip it
-        bottomG = topG; // currently eqioped gun goes to the top
-        bottomG = holderG; // top gun goes to the bottom
+    public void Equipped(){ // bottom gun is the equipped gun
+      holderG = topGunImage.texture; // the player pressed on the top gun to equip it
+      topGunImage.texture = bottomGunImage.texture; // currently equiped gun goes to the top
+      bottomGunImage.texture = holderG; // top gun goes to the bottom
+  
 
 
     }

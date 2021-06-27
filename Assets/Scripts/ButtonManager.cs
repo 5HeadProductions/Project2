e8aef65121dda3e08605911a710a11b74efc5683
Andres Maturin <idontknowwhattonamethiskhay@gmail.com>
2021-06-27 10:21:57 -0700

    Updated shop canvas

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -28,19 +28,36 @@ public class ButtonManager : MonoBehaviour
     public void LoadWeapons(){
         SceneManager.LoadScene(weaponScene);
     }
+    public void DefaultPistoEquip() // this is for the HUDUI canvas
+    {
+        if (GameObject.FindGameObjectWithTag("Primary") != null)        //if there is a different primary equipped then destroy it
+        {    
+            Destroy(GameObject.FindGameObjectWithTag("Primary"));  
+        }
+        GameObject weaponClone = Instantiate(weaponHolder.Weapons[0], weaponHolder.Weapons[0].transform.position, //Instantiate a clone of wanted weapon
+                weaponHolder.Weapons[0].transform.rotation);
+        weaponClone.transform.parent = weaponHolder.player.transform; //player gets child: weapon
+       
+        weaponClone.transform.position = weaponHolder.ARVector + weaponHolder.player.transform.position;   //assigning the newly spawned weapon The ARVector (position)
+        
+        weaponClone.GetComponentInChildren<FirePoint>().enabled = true; //firepoint is turned on bc it is off on the prefabs
+ 
+    }
 
     public void DefaultAREquip() // this is for the HUDUI canvas
     {
+        //switch statements
+      //  Equip(1);
+ 
+    }
+    public void Equip(int num) // this is for the HUDUI canvas
+    {
         if (GameObject.FindGameObjectWithTag("Primary") != null)        //if there is a different primary equipped then destroy it
-        {
-            
-            Destroy(GameObject.FindGameObjectWithTag("Primary"));
-            
+        {    
+            Destroy(GameObject.FindGameObjectWithTag("Primary"));  
         }
-        
-        GameObject weaponClone = Instantiate(weaponHolder.Weapons[0], weaponHolder.Weapons[0].transform.position, //Instantiate a clone of wanted weapon
-                weaponHolder.Weapons[0].transform.rotation);
-        Debug.Log(weaponHolder.player.transform.position);
+        GameObject weaponClone = Instantiate(weaponHolder.Weapons[num], weaponHolder.Weapons[num].transform.position, //Instantiate a clone of wanted weapon
+                weaponHolder.Weapons[num].transform.rotation);
         weaponClone.transform.parent = weaponHolder.player.transform; //player gets child: weapon
        
         weaponClone.transform.position = weaponHolder.ARVector + weaponHolder.player.transform.position;   //assigning the newly spawned weapon The ARVector (position)

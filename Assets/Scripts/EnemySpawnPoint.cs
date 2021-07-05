01c4e9bd4c42e4ff09357a6ce8e3cbd24509edad
EdenArts <ojedaeden08@gmail.com>
2021-07-05 12:11:37 -0700

    changed ranged zombie a bit and updated stats depending on the scene

--- Assets/Scripts/EnemySpawnPoint.cs
+++ Assets/Scripts/EnemySpawnPoint.cs
@@ -10,11 +10,12 @@ public class EnemySpawnPoint : MonoBehaviour
     void Start()
     {
 
-        int rand = Random.Range(1, 2);
-        int enemyRand = Random.Range(0,Enemies.Length - 1);
+        int rand = Random.Range(1, 4);      //random for amount of zombies spawned
+        int enemyRand = Random.Range(0,2);  //random # for the type of enemy in array Enemies
         
         for (int i = 0; i < rand; i++)
         {
+            Debug.Log(enemyRand);
             Instantiate(Enemies[enemyRand],transform.position,Quaternion.identity);
         }
         
